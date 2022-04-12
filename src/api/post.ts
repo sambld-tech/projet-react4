74b5008da49a0938353bca2b6ce0a2c33334e976
sambld-tech <60396897+sambld-tech@users.noreply.github.com>
2022-04-12 09:37:49 +0200

    11/04

---
+++ src/api/post.ts
@@ -0,0 +1,62 @@
+import { Post, PostContent } from './types'
+import axios from 'axios'
+import { responsiveFontSizes } from '@mui/material'
+
+const base_url = 'http://localhost:3004/posts'
+
+
+async function getPost(postID: Post['id']): Promise<Post> {
+    // get a unique post
+    // [TODO] remove this return to use a fetch API
+   
+    const response = await fetch(`${base_url}/${postID}`)
+    const data = await response.json()
+
+    return data
+    
+}
+
+async function getPosts(): Promise<Array<Post>> {
+    // get all posts
+    // [TODO] remove this return to use a fetch API
+    
+    const response = await fetch(base_url)
+    const data = await response.json()
+
+
+    return data
+
+}
+
+async function createPost(post: PostContent): Promise<Post> {
+    // create a new post
+    // [TODO] remove this return to use a fetch API
+    
+    return {
+        id: 1,
+        title: 'title',
+        userId: 1,
+        body: 'body',
+        postImageUrl: 'https://picsum.photos/1280/960',
+    }
+}
+
+async function updatePost(post: Post): Promise<Post> {
+    // update a existing post
+    // [TODO] remove this return to use a fetch API
+    return {
+        id: 1,
+        title: 'title',
+        userId: 1,
+        body: 'body',
+        postImageUrl: 'https://picsum.photos/1280/960',
+    }
+}
+
+async function deletePost(postID: Post['id']): Promise<Post['id']> {
+    // delete a existing post
+    // [TODO] remove this return to use a fetch API
+    return 1
+}
+
+export { getPost, getPosts, deletePost, updatePost, createPost }
